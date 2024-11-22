daba4a33bd4b20931d4f8056b24ba141c95e3ff0
Dinesh <dineshlegha23@gmail.com>
2024-11-22 15:36:44 +0530

    added functionality to add task

--- src/tasks.tsx
+++ src/tasks.tsx
@@ -1,1 +1,66 @@
-console.log("HELLO");
+const taskForm = document.querySelector<HTMLFormElement>(".form")!;
+
+const formInput = document.querySelector<HTMLInputElement>(".form-input");
+
+const taskListElement = document.querySelector<HTMLUListElement>(".list");
+
+type Task = {
+  description: string;
+  isCompleted: boolean;
+};
+
+const tasks: Task[] = loadTasks();
+
+tasks.forEach((task) => renderTask(task));
+
+function loadTasks(): Task[] {
+  const storedTasks = localStorage.getItem("tasks");
+  return storedTasks ? JSON.parse(storedTasks) : [];
+}
+
+function createTask(event: SubmitEvent) {
+  event.preventDefault();
+  const taskDescription = formInput?.value;
+
+  if (taskDescription) {
+    const task: Task = {
+      description: taskDescription,
+      isCompleted: false,
+    };
+    addTask(task);
+    renderTask(task);
+    updateStorage();
+
+    formInput.value = "";
+    return;
+  }
+  alert("Please enter a value");
+}
+
+function addTask(task: Task): void {
+  tasks.push(task);
+  console.log(tasks);
+}
+
+function renderTask(task: Task): void {
+  const taskElement = document.createElement("li");
+  taskElement.textContent = task.description;
+
+  const taskCheckbox = document.createElement("input");
+  taskCheckbox.type = "checkbox";
+  taskCheckbox.checked = task.isCompleted;
+
+  taskCheckbox.addEventListener("change", () => {
+    task.isCompleted = !task.isCompleted;
+    updateStorage();
+  });
+
+  taskElement.appendChild(taskCheckbox);
+  taskListElement?.appendChild(taskElement);
+}
+
+function updateStorage(): void {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+taskForm.addEventListener("submit", createTask);
